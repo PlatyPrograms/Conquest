47d5cdab1ae65efd23f7a758554a8cea4c58b490
PlatyPrograms <eric.barclay.andrews@gmail.com>
2017-05-11 20:52:44 -0400

    added backend branch, added beginning of board.c

---
+++ board.c
@@ -0,0 +1,10 @@
+class Board {
+ public:
+  getBoardState();     // 0
+  parseMove();         // 0
+ private:
+  getPieceAtNode();    // 0
+  // TODO: figure out an efficient node implementation (probably r/b tree)
+};
+
+// TODO: implement empty methods
